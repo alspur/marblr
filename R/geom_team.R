2e2f0aaa21580674f341d0f67f0ab087e2281d44
Alex Spurrier <alex.spurrier@gmail.com>
2018-01-09 09:45:11 -0500

    update geoms to illustrate big wins/losses

--- R/geom_team.R
+++ R/geom_team.R
@@ -23,7 +23,7 @@ geom_team <- function(df, team_name, team_color){
   
 }
 
-#' geom_big_games
+#' geom_big_wins
 #'
 #' @param df A dataframe resulting from calling the marble_game funciton
 #' @param team_name NCAA D1 FBS Football team name
@@ -32,11 +32,10 @@ geom_team <- function(df, team_name, team_color){
 #' @import ggplot2
 #' @import dplyr
 #'
-#' @return A ggplot geom plotting the most consequential marble changes for a team in a  given season.
+#' @return A ggplot geom plotting a team's largest win and any win that results in a gain of 40+ marbles.
 #' @export
 #'
-#'
-geom_big_games <- function(df, team_name, team_color){
+geom_big_wins <- function(df, team_name, team_color){
   
   # set vars to null to satisfy devtools::check()
   team <- NULL
@@ -46,7 +45,37 @@ geom_big_games <- function(df, team_name, team_color){
   
   geom_point(data = df %>% 
                filter(team == team_name) %>% 
-               filter(!is.na(game_flag)), 
+               filter(str_detect(game_flag, "win")), 
             aes(x =season_wk, y = marbles), color = team_color)
+  
+}
+
+
+
+#' geom_big_loss
+#'
+#' @param df A dataframe resulting from calling the marble_game funciton
+#' @param team_name NCAA D1 FBS Football team name
+#' @param team_color Color to represent team
+#' 
+#' @import ggplot2
+#' @import dplyr
+#'
+#' @return A ggplot geom plotting the biggest loss for a team or any loss that results in losing 40+ marbles.
+#' @export
+#'
+geom_big_loss <- function(df, team_name, team_color){
+  
+  # set vars to null to satisfy devtools::check()
+  team <- NULL
+  season_wk <- NULL
+  marbles <- NULL
+  game_flag <- NULL
+  
+  geom_point(data = df %>% 
+               filter(team == team_name) %>% 
+               filter(str_detect(game_flag, "loss")), 
+             aes(x =season_wk, y = marbles), 
+             color = team_color, shape = 4, size = 2)
   
 }
